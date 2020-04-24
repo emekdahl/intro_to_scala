9596ba226f69c7896d607ff44b195590ba43d452
emekdahl <emily.ekdahl@gmail.com>
2020-04-24 04:40:24 -0500

    changing mutable vars in scala

---
+++ a_scalable_language/change_mutable_var.scala
@@ -0,0 +1,8 @@
+// Create a mutable variable for Alex as player A
+var playerA: String = "Alex"
+
+// Change the point value of A♦ from 1 to 11
+aceDiamonds = 11
+
+// Calculate hand value for J♣ and A♦
+println(jackClubs + aceDiamonds)
