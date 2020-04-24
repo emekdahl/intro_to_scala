472eb665e27449835cc2481813e596a7b30fcdfa
emekdahl <emily.ekdahl@gmail.com>
2020-04-24 05:08:17 -0500

    write a 21 script in scala

--- a_scalable_language/game.scala
+++ a_scalable_language/game.scala
@@ -1,1 +1,21 @@
 println("Let's play Twenty-One!")
+
+var queenDiamonds: Int = 11
+var threeClubs: Int = 3
+var aceHearts: Int = 1
+var fiveSpades: Int = 5
+var kingHearts: Int = 12
+var jackHearts: Int = 11
+
+
+def maxHand(handA: Int, handB: Int): Int = {
+  if (handA > handB) handA
+  else handB
+}
+
+// Calculate hand values
+var handPlayerA: Int = queenDiamonds + threeClubs + aceHearts + fiveSpades
+var handPlayerB: Int = kingHearts + jackHearts
+
+// Find and print the maximum hand value
+println(maxHand(handPlayerA, handPlayerB))
