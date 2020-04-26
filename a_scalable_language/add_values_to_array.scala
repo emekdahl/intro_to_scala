9ffa0d8eea8d9a271f17011fd200799d5ffb40de
emekdahl <emily.ekdahl@gmail.com>
2020-04-26 04:35:04 -0500

    add to an array

---
+++ a_scalable_language/add_values_to_array.scala
@@ -0,0 +1,14 @@
+// Initialize player's hand and print out hands before each player hits
+hands(0) = tenClubs + fourDiamonds
+hands(1) = nineSpades + nineHearts
+hands(2) = twoClubs + threeSpades
+hands.foreach(println)
+
+// Add 5♣ to the first player's hand
+hands(0) = hands(0) + fiveClubs
+
+// Add Q♠ to the second player's hand
+hands(1) = hands(1) + queenSpades
+
+// Add K♣ to the third player's hand
+hands(2) = hands(2) + kingClubs
---
+++ a_scalable_language/create_parameterize_initialize_array.scala
@@ -0,0 +1,4 @@
+// Create, parameterize, and initialize an array for a round of Twenty-One
+val hands = Array(tenClubs + fourDiamonds,
+  nineSpades + nineHearts,
+  twoClubs + threeSpades)
