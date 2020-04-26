6f26b0fe481b5cdfee3a5752c0357156e37ee2dc
emekdahl <emily.ekdahl@gmail.com>
2020-04-26 04:47:17 -0500

    prepend to an array

---
+++ a_scalable_language/prepend_to_list.scala
@@ -0,0 +1,7 @@
+// Initialize a list with an element for each round's prize
+val prizes = List(10, 15, 20, 25, 30)
+println(prizes)
+
+// Prepend to prizes to add another round and prize
+val newPrizes = 5 :: prizes
+println(newPrizes)
