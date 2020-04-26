78f2f658a0bbc04b68447e0f18725e67985add68
emekdahl <emily.ekdahl@gmail.com>
2020-04-26 04:53:53 -0500

    concatenate arrays

---
+++ a_scalable_language/concatenate_arrays.scala
@@ -0,0 +1,6 @@
+// The original NTOA and EuroTO venue lists
+val venuesNTOA = List("The Grand Ballroom", "Atlantis Casino", "Sarrah's House")
+val venuesEuroTO = "Five Seasons Hotel" :: "The Electric Unicorn" :: Nil
+
+// Concatenate the North American and European venues
+val venuesTOWorld = venuesNTOA ::: venuesEuroTO
---
+++ a_scalable_language/initialize_with_cons_operator.scala
@@ -0,0 +1,3 @@
+// Initialize a list with an element each round's prize
+val prizes = 10 :: 15 :: 20 :: 25 :: 30 :: Nil
+println(prizes)
